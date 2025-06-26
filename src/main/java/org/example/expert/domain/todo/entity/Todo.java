38b6f34f6fa2d8c1f599efbdacf03b44f01b5307
hyohee0613 <gygml9@gmail.com>
2025-06-26 21:05:23 +0900

    Level 2-6,7 완료

--- src/main/java/org/example/expert/domain/todo/entity/Todo.java
+++ src/main/java/org/example/expert/domain/todo/entity/Todo.java
@@ -35,7 +35,7 @@ public class Todo extends Timestamped {
     @OneToMany(mappedBy = "todo", cascade = CascadeType.REMOVE)
     private List<Comment> comments = new ArrayList<>();
 
-    @OneToMany(mappedBy = "todo")
+    @OneToMany(mappedBy = "todo", cascade = CascadeType.PERSIST)
     private List<Manager> managers = new ArrayList<>();
 
     public Todo(String title, String contents, WeatherType weather, User user) {
